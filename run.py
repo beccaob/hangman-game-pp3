d2f16688ef78c893e40a062ca2910abcc0ee40dc
beccaob <rob3651@outlook.com>
2023-11-21 00:20:34 +0000

    fix issues in line breaker

--- run.py
+++ run.py
@@ -27,21 +27,21 @@ def hangman():
 
 # username function - error if requirements not met. Print to console when met
 def welcome():
-    print("Welcome to Hangman! This is a gussing game where you have a limited number of lives. Do you think you have what it takes?/n")
+    print("Welcome to Hangman! This is a gussing game where you have a limited number of lives. Do you think you have what it takes?\n")
     while True:
         username = input("Enter your username below to get started! (1-8 characters, no numbers or special characters):\n")
 
         if 1 <= len(username) <= 8 and username.isalpha():
             break
         else:
-            print("Username error, please try again./n")
+            print("Username error, please try again.\n")
 
-    print(f"Hello, {username}! Welcome to Hangman./n")
+    print(f"Hello, {username}! Welcome to Hangman.\n")
     return username
 
 # game options
 def display_menu():
-    print("Please select an option below:/n")
+    print("Please select an option below:\n")
     print("1. Start Game")
     print("2. Exit Game/n")
 
@@ -51,7 +51,7 @@ def get_user_choice():
     return choice
 
 def start_game(username):
-    objective = "The words used in this game are popular musical instruments. You have 10 lives. Good luck!/n"
+    objective = "The words used in this game are popular musical instruments. You have 10 lives. Good luck!\n"
     print(objective)
     
     # choose random word and set guessed letters to an empty set
@@ -64,32 +64,32 @@ def start_game(username):
         guess = get_user_guess()
 
         if guess in guessed_letters:
-            print("Oops! You've already guessed that letter. Please try again./n")
+            print("Oops! You've already guessed that letter. Please try again.\n")
             continue  # Skip the rest of the loop and get a new guess
 
         guessed_letters.add(guess)
 
         if guess in word_to_guess:
             if all(letter in guessed_letters for letter in word_to_guess):
-                print(f"Congratulations, {username}! You guessed the word: {word_to_guess}/n")
+                print(f"Congratulations, {username}! You guessed the word: {word_to_guess}\n")
                 break
         else:
             lives -= 1
-            print(f"Wrong guess! Lives left: {lives}/n")
+            print(f"Wrong guess! Lives left: {lives}\n")
 
             if lives == 0:
-                print(f"Oops! You ran out of lives./n The correct word was: {word_to_guess}./nBetter luck next time!")
+                print(f"Oops! You ran out of lives./n The correct word was: {word_to_guess}.\nBetter luck next time!")
 
     end_game(username)
 
 # get single letter input before game begins, error if requirements not met
 def get_user_guess():
     while True:
-        guess = input("Guess a letter: /n").lower()
+        guess = input("Guess a letter: \n").lower()
         if guess.isalpha() and len(guess) == 1:
             return guess
         else:
-            print("Invalid input. Please enter a single letter./n")
+            print("Invalid input. Please enter a single letter.\n")
 
 # users progress of lives and guesses 
 def display_word_with_lives(word, guessed_letters, lives):
@@ -97,20 +97,20 @@ def display_word_with_lives(word, guessed_letters, lives):
 
 # end of game options
 def end_game(username):
-    print("Game Over! Want to try again?/n")
-    print("Select an option:/n")
+    print("Game Over! Want to try again?\n")
+    print("Select an option:\n")
     print("1. Restart")
-    print("2. Quit To Home/n")
+    print("2. Quit To Home\n")
     choice = get_user_choice()
 
     # action to be taken after user choice
     if choice == '1':
         start_game(username)
     elif choice == '2':
-        print(f"Sure thing, {username}! Returning home../n")
+        print(f"Sure thing, {username}! Returning home..\n")
         return
     else:
-        print("Invalid choice. Returning Home.../n")
+        print("Invalid choice. Returning Home...\n")
         return   
 
 # checks if script is being run as main program and not imported 
@@ -128,4 +128,4 @@ if __name__ == "__main__":
             print(f"Goodbye, {username}! Hope to see you again soon!")
             break
         else:
-            print("Invalid choice. Please enter 1 or 2./n")
+            print("Invalid choice. Please enter 1 or 2.\n")
